65a7b12290df2de8d60ca4e28f4e1fbf80f6084f
IxxyDev <denisenkoforwork@gmail.com>
2025-07-15 23:17:11 +0300

    fix: package.json and some syntax errors

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ import './lib/report.js';
 import { wrap } from './lib/timer.js';
 import { hotLoop } from './example/hot.js';
 
-const timed = wrap();
+const timed = wrap(hotLoop, 'hotLoop');
 for (let i = 0; i < 1_000; i++) {
   timed();
 }
